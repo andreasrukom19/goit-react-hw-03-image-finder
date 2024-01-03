4135692f39d1878517a211d887ffeef4cda174c0
Андрій <rukom@ukr.net>
2024-01-03 19:16:54 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,7 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import React from 'react';
 import css from '../ImageGallery/ImageGallery.module.css';
 
-export const ImageGallery = ({images}) => {
+export const ImageGallery = ({images, handleShowImage}) => {
   return (
     <ul className={css['image-gallery']}>
       {images?.map(image => (
@@ -11,6 +11,7 @@ export const ImageGallery = ({images}) => {
           id={image.id}
           url={image.webformatURL}
           description={image.user}
+          handleShowImage={handleShowImage}
         />))}
     </ul>
   )
