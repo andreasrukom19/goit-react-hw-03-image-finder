baa256fa7ee0dbedc65270dbb7fa2d22068c58fa
Андрій <rukom@ukr.net>
2024-01-03 19:16:51 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import css from '../ImageGalleryItem/ImageGalleryItem.module.css';
 
-export const ImageGalleryItem = ({ id, url, description }) => {
+export const ImageGalleryItem = ({ id, url, description, handleShowImage }) => {
   return (
-    <li id={id} className={css['image-gallery-item']}>
-      <img src={url} alt={description} className={css['gallery-item-image']} />
+    <li className={css['image-gallery-item']}>
+      <img
+        src={url}
+        alt={description}
+        className={css['gallery-item-image']}
+        onClick={() => handleShowImage(id)}
+      />
     </li>
   )
 }
